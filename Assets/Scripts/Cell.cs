3e1771ca2d30e45645509652ae561877411438ca
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-13 12:19:26 +0300

    Added constructor which uses scriptable object

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    Color color;
-    bool hasAnt;
-    bool isPasable;
-    float food;
-    float foodPheromone;
-    float homePheromone;
+    public Color color;
+    public bool hasAnt;
+    public bool isPasable;
+    public float food;
+    public float foodPheromone;
+    public float homePheromone;
+
+    public Cell(CellType cellType)
+    {
+        color = cellType.color;
+        isPasable = cellType.isPasable;
+        food = cellType.food;
+    }
 }
