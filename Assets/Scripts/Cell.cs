bea2ee4869b3f8f5dbb2cf23cd4dfefa49db26c2
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-14 15:52:24 +0300

    Added setting and removing ant from the cell

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Cell
 {
     public int x, y;
+    private Color baseColor;
     public Color color;
     public bool hasAnt;
     public bool isPasable;
@@ -10,16 +11,34 @@ public class Cell
     public float foodPheromone;
     public float homePheromone;
     public CellFlag cellFlag;
+    public Ant ant;
     public Cell(CellType cellType, int x, int y)
     {
         color = cellType.color;
+        baseColor = cellType.color;
         isPasable = cellType.isPasable;
         food = cellType.food;
         cellFlag = cellType.cellFlag;
         this.x = x;
         this.y = y;
     }
-
+    public void RemoveAnt()
+    {
+        ant = null;
+        hasAnt = false;
+        color = baseColor;
+    }
+    public void SetAnt(Ant ant)
+    {
+        if (ant == null) 
+        {
+            Debug.LogWarning($"Trying to set empty ant to cell with cords {x} {y}");
+            return; 
+        }
+        this.ant = ant;
+        hasAnt = true;
+        color = ant.color;
+    }
     public WorldView CellToWorldView()
     {
         return new WorldView(x,y,isPasable,foodPheromone,homePheromone,cellFlag,food);
