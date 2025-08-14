01763ff230e7d1c6a3cb8037492d26e2a9aa04d0
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-14 15:31:55 +0300

    Created WorldView struct

---
+++ Assets/Scripts/WorldView.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public readonly struct WorldView
+{
+    public readonly int x, y;
+    public readonly bool isPasable;
+    public readonly float foodPheromone, homePheromone;
+    public readonly CellFlag cellType;
+
+    public WorldView(int x, int y, bool isPasable, float foodPheromone, float homePheromone, CellFlag cellType)
+    {
+        this.x = x;
+        this.y = y;
+        this.isPasable = isPasable;
+        this.foodPheromone = foodPheromone;
+        this.homePheromone = homePheromone;
+        this.cellType = cellType;
+    }
+}
