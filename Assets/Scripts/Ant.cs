33cfcb3c9c035d42e55de89aa21f3652a88ed15c
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-14 18:04:32 +0300

    Removed HasFood property

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -3,13 +3,7 @@ using UnityEngine;
 public class Ant
 {
     private bool hasFood = false;
-    public bool HasFood { get
-        {
-
-            return hasFood;
-        } private set { 
-            hasFood = value;
-        } }
+  
     float foodPheromone = 0;
     float foodAmount = 0;
     float maxFood;
