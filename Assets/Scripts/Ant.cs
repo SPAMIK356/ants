1b7f4f3cb5eacb16ea709d45c503660e02c7eb80
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-14 18:16:44 +0300

    Added food manipulation functions

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class Ant
 {
-    private bool hasFood = false;
+    public bool hasFood { get; private set; } = false;
   
-    float foodPheromone = 0;
+    public float foodPheromone = 0;
+    public float homePheromone = 0;
     float foodAmount = 0;
     float maxFood;
     public Color color;
@@ -15,5 +16,31 @@ public class Ant
         maxFood = template.maxFood;
         this.x = x;
         this.y = y;
+    }
+    public float GetFood(float sourceFoodAmount)
+    {
+        if (sourceFoodAmount <= 0) 
+        {
+            Debug.LogWarning($"Ant with cords {x} {y} trying to get food from empty source!");
+            return 0; 
+        }
+        hasFood = true;
+        foodAmount += Mathf.Clamp(sourceFoodAmount, 0, maxFood);
+        foodPheromone = 1;
+        return sourceFoodAmount-foodAmount;
+        
+    }
+    public float GiveFood()
+    {
+        if (!hasFood)
+        {
+            Debug.LogWarning($"Ant with cords {x} {y} trying to give food despite not having it!");
+            return 0;
+        }
+
+        var givenFood = foodAmount;
+        foodAmount = 0;
+        hasFood = false;
+        return givenFood;
     }
 }
