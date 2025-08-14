1e158a1abe52b6055a7777017f7663a5dd96645f
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-14 18:49:34 +0300

    Fnished food search logic

---
+++ Assets/Scripts/RegularAntDecide.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class RegularAntDecide : IDecide
+{
+    public Intent Decide(WorldView[] worldViews, Ant ant) 
+    {
+
+        float[] weights = new float[worldViews.Length];
+
+        float weightSum = 0;
+        if (ant.hasFood)
+            for (int i = 0; i < worldViews.Length; i++)
+            {
+                //Логіка повернення їжі
+            }
+        else
+            for(int i = 0; i < worldViews.Length; i++)
+            {
+                if (worldViews[i].cellType == CellFlag.FoodSource) //Якщо є джерело їжі
+                {
+                    return new Intent(worldViews[i].x, worldViews[i].y, ant, new GetFood()); //Взяти їжу
+                }
+                if (!worldViews[i].isPasable)  //Якщо клітина непроходима
+                { 
+                    weights[i] = 0;     //Вага нуль
+                    continue;
+                }
+                weights[i] = 1+worldViews[i].foodPheromone;
+                weightSum += weights[i];
+            }
+        float choice = Random.Range(0, weightSum);
+        float num = 0;
+        for (int i = 0; i < worldViews.Length; i++) 
+        { 
+            num+= weights[i];
+            if (num > choice) return new Intent(worldViews[i].x,worldViews[i].y, ant, new Move());
+        }
+        return new Intent(ant.x,ant.y,ant, new Idle());
+    }
+
+}
