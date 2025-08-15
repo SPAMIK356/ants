65cfc8e64d3b3e4d2152908bb89be48f6f36c898
SPAMIK356 <maximbondarenko20@gmail.com>
2025-08-15 20:51:40 +0300

    Added pheromone droping when moving

--- Assets/Scripts/Actions/Move.cs
+++ Assets/Scripts/Actions/Move.cs
@@ -4,7 +4,15 @@ public class Move : IAction
 {
     public void DoAction(Intent intent, Cell targetCell, Cell startCell)
     {
-        startCell.RemoveAnt();
+        if(intent.ant.hasFood)
+        {
+            startCell.foodPheromone += intent.ant.DropFoodPheromones();
+        }
+        else
+        {
+            startCell.homePheromone += intent.ant.DropFoodPheromones();
+        }
+            startCell.RemoveAnt();
         targetCell.SetAnt(intent.ant);
         intent.ant.x = intent.x;
         intent.ant.y = intent.y;
